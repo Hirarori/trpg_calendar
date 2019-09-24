1c171439e142bf8704e9720f1d7d075850a7b73d
Yuta Natori <natori@appirits.com>
2019-09-24 18:56:37 +0900

    削除：余分に存在したnameのmigrateファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190924055511) do
+ActiveRecord::Schema.define(version: 20190924095227) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20190924055511) do
     t.string "nickname"
     t.string "location"
     t.string "image"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
