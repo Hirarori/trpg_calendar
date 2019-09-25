707b25caff17157724dbabdadd17ac3674951b5f
Yuta Natori <natori@appirits.com>
2019-09-25 17:20:16 +0900

    追加：User#Show画面予定ボタン

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   devise_for :users,  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
                                     registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: :show
 end
