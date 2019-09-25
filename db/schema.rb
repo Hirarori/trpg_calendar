0832b0e361ebeb5a65e6410f267f5f8baf06c297
Yuta Natori <natori@appirits.com>
2019-09-25 14:16:20 +0900

    生成:Planモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190925011640) do
+ActiveRecord::Schema.define(version: 20190925051017) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.text "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_plans_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
