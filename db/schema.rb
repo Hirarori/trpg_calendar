04af6910dbdf3086adc61b5554a67f77400cd55b
Yuta Natori <natori@appirits.com>
2019-09-25 10:14:40 +0900

    ローカルでの変数修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190924095227) do
+ActiveRecord::Schema.define(version: 20190924050734) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
