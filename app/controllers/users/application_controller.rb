24454d93c059c0128cf63636e10af1b9b3f46c89
Yuta Natori <natori@appirits.com>
2019-09-25 14:26:19 +0900

    生成：Plansコントローラー

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -1,5 +1,7 @@
 class Users::ApplicationController  < ApplicationController
   before_action :authenticate_user!
   def show
+    @user = User.find(params[:id])
+    @plans = @user.plans.paginate(page: params[:page])
   end
 end
