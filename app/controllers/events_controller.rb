e19439a53a7a8c32af6a9bb9ace7eb49c0dd301b
Yuta Natori <natori@appirits.com>
2019-09-26 10:30:08 +0900

    削除：登録・ログインフォーム

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,17 +10,28 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-    @event = Event.find_by(id: params[:id])
-    @user = @event.user
+      @event = Event.find_by(id: params[:id])
+      @user = @event.user
   end
 
   # GET /events/new
   def new
-    @event = Event.new
+    if user_signed_in?
+      @event = Event.new
+    else
+      flash[:caution] = "ログインしてください"
+      redirect_to new_user_session_path
+    end
   end
 
   # GET /events/1/edit
   def edit
+    if user_signed_in?
+      @event = Event.find_by(id: params[:id])
+    else
+      flash[:caution] = "ログインしてください"
+      redirect_to new_user_session_path
+    end
   end
 
   # POST /events
@@ -49,26 +60,36 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        flash[:caution] = "予定を編集しました"
-        format.html { redirect_to @event }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+    if user_signed_in?
+      respond_to do |format|
+        if @event.update(event_params)
+          flash[:caution] = "予定を編集しました"
+          format.html { redirect_to @event }
+          format.json { render :show, status: :ok, location: @event }
+        else
+          format.html { render :edit }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      flash[:caution] = "ログインしてください"
+      redirect_to new_user_session_path
     end
   end
 
   # DELETE /events/1
   # DELETE /events/1.json
   def destroy
-    @event.destroy
-    respond_to do |format|
-      flash[:caution] = "予定を削除しました"
-      format.html { redirect_to events_url }
-      format.json { head :no_content }
+    if user_signed_in?
+      @event.destroy
+      respond_to do |format|
+        flash[:caution] = "予定を削除しました"
+        format.html { redirect_to events_url }
+        format.json { head :no_content }
+      end
+    else
+      flash[:caution] = "ログインしてください"
+      redirect_to new_user_session_path
     end
   end
 
